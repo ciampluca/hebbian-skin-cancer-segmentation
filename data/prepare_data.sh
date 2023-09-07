6d6caf2824f16064042094f96aeded7520243f3f
Gabriele Lagani <gabriele.lagani@gmail.com>
2023-09-07 10:23:40 +0200

    Update prepare_data.sh

--- data/prepare_data.sh
+++ data/prepare_data.sh
@@ -8,13 +8,14 @@ fi
 
 PH2_DIR="${DATASET_ROOT}/PH2"
 
-PH2_URL="https://drive.google.com/uc?id=16eMw7LWKbnJ8wR6Hk-YC1OJuu7cZlrzp"
+PH2_URL="./dataset_url.txt"
 
 
 # PH2 DATASET
 if [[ ! -e "${PH2_DIR}" ]]; then
     echo "Downloading, extracting, and preparing: PH2 dataset"
-    gdown "${PH2_URL}" -O ${DATASET_ROOT}
+    read -r line < ${PH2_URL}
+    gdown "${line}" -O ${DATASET_ROOT}
     unrar x ${DATASET_ROOT}/PH2Dataset.rar && rm ${DATASET_ROOT}/PH2Dataset.rar
     python prepare_PH2.py
     rm -rf ${DATASET_ROOT}/PH2Dataset
