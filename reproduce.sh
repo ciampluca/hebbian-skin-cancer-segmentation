4849287cd92b83361248d11b940c921d301bf17e
luca-cnr <luca.ciampi@isti.cnr.it>
2023-09-22 17:34:05 +0200

    added conf for brainMRI dataset; changes in reproduce

--- reproduce.sh
+++ reproduce.sh
@@ -15,6 +15,11 @@ EXPS=(
     ph2/hunet-hpca_ft
     #ph2/hunet2-hpca
     #ph2/hunet2-hpca_ft
+    brainMRI/unet
+    brainMRI/hunet-hpca
+    brainMRI/hunet-hpca_ft
+    #brainMRI/hunet2-hpca
+    #brainMRI/hunet2-hpca_ft
 )
 
 # Train & Evaluate (k-cross validation)
@@ -28,6 +33,7 @@ done
 # Test 
 for REP in $(seq $(( $START_REP )) $(( $REPS - 1 ))); do
     for EXP in ${EXPS[@]}; do
-        CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py runs/experiment=$EXP/inv_temp-1/run-$REP --data-root data/PH2 --in-memory True --device cpu
+        CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py runs/experiment=$EXP/inv_temp-1/run-$REP --data-root data/PH2 --in-memory True
+        CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py runs/experiment=$EXP/inv_temp-1/run-$REP --data-root data/BrainRMI --in-memory False
     done
 done
