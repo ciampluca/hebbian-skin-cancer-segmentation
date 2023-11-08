1a46cb2aac7f19e8ec2ab895ebc2fe8e94560408
luca-cnr <luca.ciampi@isti.cnr.it>
2023-11-08 11:41:02 +0100

    bugs fixed; updated show; changed regime values

--- reproduce_hebbian.sh
+++ reproduce_hebbian.sh
@@ -26,10 +26,10 @@ EXPS=(
     # ph2/hunet-hpca_ft
     # ph2/hunet-hpca_t
     # ph2/hunet-hpca_t_ft
-    ph2/hfcn32s-hpca
-    ph2/hfcn32s-hpca_ft
-    ph2/hfcn32s-hpca_t
-    ph2/hfcn32s-hpca_t_ft
+    # ph2/hfcn32s-hpca
+    # ph2/hfcn32s-hpca_ft
+    # ph2/hfcn32s-hpca_t
+    # ph2/hfcn32s-hpca_t_ft
     # #ph2/hunet2-hpca_ft
     # #ph2/hunet2-hpca_t_ft
     # ph2/hunet-swta
@@ -49,20 +49,20 @@ EXPS=(
     # isic2016/hunet-hpca_ft
     # isic2016/hunet-hpca_t
     # isic2016/hunet-hpca_t_ft
-    isic2016/hfcn32s-hpca
-    isic2016/hfcn32s-hpca_ft
-    isic2016/hfcn32s-hpca_t
-    isic2016/hfcn32s-hpca_t_ft
+    # isic2016/hfcn32s-hpca
+    # isic2016/hfcn32s-hpca_ft
+    # isic2016/hfcn32s-hpca_t
+    # isic2016/hfcn32s-hpca_t_ft
     #isic2016/hunet2-hpca_ft
     #isic2016/hunet2-hpca_t_ft
     # isic2016/hunet-swta
     # isic2016/hunet-swta_ft
     # isic2016/hunet-swta_t
     # isic2016/hunet-swta_t_ft
-    isic2016/hfcn32s-swta
-    isic2016/hfcn32s-swta_ft
-    isic2016/hfcn32s-swta_t
-    isic2016/hfcn32s-swta_t_ft
+    # isic2016/hfcn32s-swta
+    # isic2016/hfcn32s-swta_ft
+    # isic2016/hfcn32s-swta_t
+    # isic2016/hfcn32s-swta_t_ft
     #isic2016/hunet2-swta_ft
     #isic2016/hunet2-hpca_ft
     #################################
@@ -72,20 +72,20 @@ EXPS=(
     # kvasirSEG/hunet-hpca_ft
     # kvasirSEG/hunet-hpca_t
     # kvasirSEG/hunet-hpca_t_ft
-    kvasirSEG/hfcn32s-hpca
-    kvasirSEG/hfcn32s-hpca_ft
-    kvasirSEG/hfcn32s-hpca_t
-    kvasirSEG/hfcn32s-hpca_t_ft
+    # kvasirSEG/hfcn32s-hpca
+    # kvasirSEG/hfcn32s-hpca_ft
+    # kvasirSEG/hfcn32s-hpca_t
+    # kvasirSEG/hfcn32s-hpca_t_ft
     #kvasirSEG/hunet2-hpca_ft
     #kvasirSEG/hunet2-hpca_t_ft
     # kvasirSEG/hunet-swta
     # kvasirSEG/hunet-swta_ft
     # kvasirSEG/hunet-swta_t
     # kvasirSEG/hunet-swta_t_ft
-    kvasirSEG/hfcn32s-swta
-    kvasirSEG/hfcn32s-swta_ft
-    kvasirSEG/hfcn32s-swta_t
-    kvasirSEG/hfcn32s-swta_t_ft
+    # kvasirSEG/hfcn32s-swta
+    # kvasirSEG/hfcn32s-swta_ft
+    # kvasirSEG/hfcn32s-swta_t
+    # kvasirSEG/hfcn32s-swta_t_ft
     #kvasirSEG/hunet2-swta_ft
     #kvasirSEG/hunet2-hpca_ft
     #################################
@@ -95,20 +95,20 @@ EXPS=(
     # datasciencebowl2018/hunet-hpca_ft
     # datasciencebowl2018/hunet-hpca_t
     # datasciencebowl2018/hunet-hpca_t_ft
-    datasciencebowl2018/hfcn32s-hpca
-    datasciencebowl2018/hfcn32s-hpca_ft
-    datasciencebowl2018/hfcn32s-hpca_t
-    datasciencebowl2018/hfcn32s-hpca_t_ft
+    # datasciencebowl2018/hfcn32s-hpca
+    # datasciencebowl2018/hfcn32s-hpca_ft
+    # datasciencebowl2018/hfcn32s-hpca_t
+    # datasciencebowl2018/hfcn32s-hpca_t_ft
     #datasciencebowl2018/hunet2-hpca_ft
     #datasciencebowl2018/hunet2-hpca_t_ft
     # datasciencebowl2018/hunet-swta
     # datasciencebowl2018/hunet-swta_ft
     # datasciencebowl2018/hunet-swta_t
     # datasciencebowl2018/hunet-swta_t_ft
-    datasciencebowl2018/hfcn32s-swta
-    datasciencebowl2018/hfcn32s-swta_ft
-    datasciencebowl2018/hfcn32s-swta_t
-    datasciencebowl2018/hfcn32s-swta_t_ft
+    # datasciencebowl2018/hfcn32s-swta
+    # datasciencebowl2018/hfcn32s-swta_ft
+    # datasciencebowl2018/hfcn32s-swta_t
+    # datasciencebowl2018/hfcn32s-swta_t_ft
     #datasciencebowl2018/hunet2-swta_ft
     #datasciencebowl2018/hunet2-hpca_ft
 )
@@ -117,7 +117,7 @@ EXPS=(
 for REP in $(seq $(( $START_REP )) $(( $REPS - 1 ))); do    # Multiple repetitions of the same experiment, varying the validation bucket
     for EXP in ${EXPS[@]}; do
         case $EXP in
-            */hunet-swta*)
+            */*-swta*)
                 case $EXP in
                     ph2*)
                         CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python train.py experiment=$EXP data.train.cross_val_bucket_validation_index=$REP model.hebb.k=$INV_TEMP_PH2;;
@@ -141,28 +141,28 @@ for REP in $(seq $(( $START_REP )) $(( $REPS - 1 ))); do
         case $EXP in 
             ph2*)
                 case $EXP in
-                    */hunet-swta*)
+                    */*-swta*)
                         CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-$INV_TEMP_PH2/regime-1.0/run-$REP --data-root $EVAL_DATA_ROOT/PH2 --in-memory True;;
                     *)
                         CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-1/regime-1.0/run-$REP --data-root $EVAL_DATA_ROOT/PH2 --in-memory True;;
                 esac;;
             isic2016*)
                 case $EXP in
-                    */hunet-swta*)
+                    */*-swta*)
                         CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-$INV_TEMP_ISIC2016/regime-1.0/run-$REP --data-root $EVAL_DATA_ROOT/ISIC2016 --in-memory True;;
                     *)
                         CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-1/regime-1.0/run-$REP --data-root $EVAL_DATA_ROOT/ISIC2016 --in-memory True;;
                 esac;;
             kvasirSEG*)
                 case $EXP in
-                    */hunet-swta*)
+                    */*-swta*)
                         CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-$INV_TEMP_KvasirSEG/regime-1.0/run-$REP --data-root $EVAL_DATA_ROOT/KvasirSEG --in-memory True;;
                     *)
                         CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-1/regime-1.0/run-$REP --data-root $EVAL_DATA_ROOT/KvasirSEG --in-memory True;;
                 esac;;
             datasciencebowl2018*)
                 case $EXP in
-                    */hunet-swta*)
+                    */*-swta*)
                         CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-$INV_TEMP_DataScienceBowl2018/regime-1.0/run-$REP --data-root $EVAL_DATA_ROOT/DataScienceBowl2018 --in-memory True;;
                     *)
                         CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-1/regime-1.0/run-$REP --data-root $EVAL_DATA_ROOT/DataScienceBowl2018 --in-memory True;;
--- reproduce_regimes.sh
+++ reproduce_regimes.sh
@@ -20,14 +20,11 @@ INV_TEMP_KvasirSEG=5        # to be set accordingly, used by SWTA
 INV_TEMP_DataScienceBowl2018=10        # to be set accordingly, used by SWTA
 
 REGIMES=(
-    0.01
-    0.02
-    0.03
-    0.04
-    0.05
-    0.1
-    0.25
-    0.5
+    #0.05
+    #0.1
+    #0.25
+    #0.5
+    0.75
     #1.0     # it should be the same of the one in reproduce_hebbian script
 )
 
@@ -37,69 +34,69 @@ EXPS=(
     #################################
     ph2/unet
     ph2/fcn32s
-    # ph2/hunet-hpca_ft
-    # ph2/hunet-hpca_t_ft
-    ph2/hfcn32s-hpca_ft
-    ph2/hfcn32s-hpca_t_ft
-    #ph2/hunet2-hpca_ft
-    #ph2/hunet2-hpca_t_ft
-    ph2/hunet-swta_ft
-    ph2/hunet-swta_t_ft
-    ph2/hfcn32s-swta_ft
-    ph2/hfcn32s-swta_t_ft
-    #ph2/hunet2-swta_ft
-    #ph2/hunet2-hpca_ft
+    ph2/hunet-hpca_ft
+    ph2/hunet-hpca_t_ft
+    # ph2/hfcn32s-hpca_ft
+    # ph2/hfcn32s-hpca_t_ft
+    # #ph2/hunet2-hpca_ft
+    # #ph2/hunet2-hpca_t_ft
+    # ph2/hunet-swta_ft
+    # ph2/hunet-swta_t_ft
+    # ph2/hfcn32s-swta_ft
+    # ph2/hfcn32s-swta_t_ft
+    # #ph2/hunet2-swta_ft
+    # #ph2/hunet2-hpca_ft
     #################################
     # ISIC2016 Dataset
     #################################
-    isic2016/unet
-    isic2016/fcn32s
-    isic2016/hunet-hpca_ft
-    isic2016/hunet-hpca_t_ft
-    isic2016/hfcn32s-hpca_ft
-    isic2016/hfcn32s-hpca_t_ft
-    #isic2016/hunet2-hpca_ft
-    #isic2016/hunet2-hpca_t_ft
-    isic2016/hunet-swta_ft
-    isic2016/hunet-swta_t_ft
-    isic2016/hfcn32s-swta_ft
-    isic2016/hfcn32s-swta_t_ft
-    #isic2016/hunet2-swta_ft
-    #isic2016/hunet2-hpca_ft
+    # isic2016/unet
+    # isic2016/fcn32s
+    # isic2016/hunet-hpca_ft
+    # isic2016/hunet-hpca_t_ft
+    # isic2016/hfcn32s-hpca_ft
+    # isic2016/hfcn32s-hpca_t_ft
+    # #isic2016/hunet2-hpca_ft
+    # #isic2016/hunet2-hpca_t_ft
+    # isic2016/hunet-swta_ft
+    # isic2016/hunet-swta_t_ft
+    # isic2016/hfcn32s-swta_ft
+    # isic2016/hfcn32s-swta_t_ft
+    # #isic2016/hunet2-swta_ft
+    # #isic2016/hunet2-hpca_ft
     #################################
     # KvasirSEG Dataset
     #################################
-    kvasirSEG/unet
-    kvasirSEG/fcn32s
-    kvasirSEG/hunet-hpca_ft
-    kvasirSEG/hunet-hpca_t_ft
-    kvasirSEG/hfcn32s-hpca_ft
-    kvasirSEG/hfcn32s-hpca_t_ft
-    #kvasirSEG/hunet2-hpca_ft
-    #kvasirSEG/hunet2-hpca_t_ft
-    kvasirSEG/hunet-swta_ft
-    kvasirSEG/hunet-swta_t_ft
-    kvasirSEG/hfcn32s-swta_ft
-    kvasirSEG/hfcn32s-swta_t_ft
-    #kvasirSEG/hunet2-swta_ft
-    #kvasirSEG/hunet2-hpca_ft
+    # kvasirSEG/unet
+    # kvasirSEG/fcn32s
+    # kvasirSEG/hunet-hpca_ft
+    # kvasirSEG/hunet-hpca_t_ft
+    # kvasirSEG/hfcn32s-hpca_ft
+    # kvasirSEG/hfcn32s-hpca_t_ft
+    # #kvasirSEG/hunet2-hpca_ft
+    # #kvasirSEG/hunet2-hpca_t_ft
+    # kvasirSEG/hunet-swta_ft
+    # kvasirSEG/hunet-swta_t_ft
+    # kvasirSEG/hfcn32s-swta_ft
+    # kvasirSEG/hfcn32s-swta_t_ft
+    # #kvasirSEG/hunet2-swta_ft
+    # #kvasirSEG/hunet2-hpca_ft
     #################################
     # DataScienceBowl2018 Dataset
     #################################
-    datasciencebowl2018/unet
-    datasciencebowl2018/fcn32s
-    datasciencebowl2018/hunet-hpca_ft
-    datasciencebowl2018/hunet-hpca_t_ft
-    datasciencebowl2018/hfcn32s-hpca_ft
-    datasciencebowl2018/hfcn32s-hpca_t_ft
-    #datasciencebowl2018/hunet2-hpca_ft
-    #datasciencebowl2018/hunet2-hpca_t_ft
-    datasciencebowl2018/hunet-swta_ft
-    datasciencebowl2018/hunet-swta_t_ft
-    datasciencebowl2018/hfcn32s-swta_ft
-    datasciencebowl2018/hfcn32s-swta_t_ft
-    #datasciencebowl2018/hunet2-swta_ft
-    #datasciencebowl2018/hunet2-hpca_ft
+    # datasciencebowl2018/unet
+    # datasciencebowl2018/fcn32s
+    # datasciencebowl2018/hunet-hpca_ft
+    # datasciencebowl2018/hunet-hpca_t_ft
+    # datasciencebowl2018/hfcn32s-hpca_ft
+    # datasciencebowl2018/hfcn32s-hpca_t_ft
+    # #datasciencebowl2018/hunet2-hpca_ft
+    # #datasciencebowl2018/hunet2-hpca_t_ft
+    # datasciencebowl2018/hunet-swta_ft
+    # datasciencebowl2018/hunet-swta_t_ft
+    # datasciencebowl2018/hfcn32s-swta_ft
+    # datasciencebowl2018/hfcn32s-swta_t_ft
+    # #datasciencebowl2018/hunet2-swta_ft
+    # #datasciencebowl2018/hunet2-hpca_ft
 )
 
 # Train & Evaluate (k-cross validation)
@@ -107,7 +104,7 @@ for R in ${REGIMES[@]}; do
     for REP in $(seq $(( $START_REP )) $(( $REPS - 1 ))); do    # Multiple repetitions of the same experiment, varying the validation bucket
         for EXP in ${EXPS[@]}; do
             case $EXP in
-                */hunet-swta*)
+                */*-swta*)
                     case $EXP in
                         ph2*)
                             CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python train.py experiment=$EXP data.train.cross_val_bucket_validation_index=$REP data.train.smpleff_regime=$R model.hebb.k=$INV_TEMP_PH2;;
@@ -133,28 +130,28 @@ for R in ${REGIMES[@]}; do
             case $EXP in 
                 ph2*)
                     case $EXP in
-                        */hunet-swta*)
+                        */*-swta*)
                             CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-$INV_TEMP_PH2/regime-$R/run-$REP --data-root $EVAL_DATA_ROOT/PH2 --in-memory True;;
                         *)
                             CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-1/regime-$R/run-$REP --data-root $EVAL_DATA_ROOT/PH2 --in-memory True;;
                     esac;;
                 isic2016*)
                     case $EXP in
-                        */hunet-swta*)
+                        */*-swta*)
                             CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-$INV_TEMP_ISIC2016/regime-$R/run-$REP --data-root $EVAL_DATA_ROOT/ISIC2016 --in-memory True;;
                         *)
                             CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-1/regime-$R/run-$REP --data-root $EVAL_DATA_ROOT/ISIC2016 --in-memory True;;
                     esac;;
                 kvasirSEG*)
                     case $EXP in
-                        */hunet-swta*)
+                        */*-swta*)
                             CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-$INV_TEMP_KvasirSEG/regime-$R/run-$REP --data-root $EVAL_DATA_ROOT/KvasirSEG --in-memory True;;
                         *)
                             CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-1/regime-$R/run-$REP --data-root $EVAL_DATA_ROOT/KvasirSEG --in-memory True;;
                     esac;;
                 datasciencebowl2018*)
                     case $EXP in
-                        */hunet-swta*)
+                        */*-swta*)
                             CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-$INV_TEMP_DataScienceBowl2018/regime-$R/run-$REP --data-root $EVAL_DATA_ROOT/DataScienceBowl2018 --in-memory True;;
                         *)
                             CUDA_VISIBLE_DEVICES=$GPU HYDRA_FULL_ERROR=1 python evaluate.py $EVAL_EXP_ROOT/experiment=$EXP/inv_temp-1/regime-$R/run-$REP --data-root $EVAL_DATA_ROOT/DataScienceBowl2018 --in-memory True;;
