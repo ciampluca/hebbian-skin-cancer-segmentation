Bugs fixed in hebbian models and backprop; bugs fixed in pseudolabeling; added conf;other bugs fixed

models/HFCN32s.py
@@ -9,8 +9,10 @@ default_hebb_params = dict(w_nrm=True, mode='swta', k=0.02, patchwise=True, cont
 
 
 class HFCN32s(nn.Module):
+
     def __init__(self, cfg, **kwargs):
         super(HFCN32s, self).__init__()
+
         hebb_params = default_hebb_params
         if hasattr(cfg.model, 'hebb'):
             hebb_params = dict(
@@ -32,6 +34,10 @@ class HFCN32s(nn.Module):
     def forward(self, x):
         return self.net(x)
     
+    def local_update(self):
+        for m in self.net.modules():
+            if hasattr(m, 'local_update'): m.local_update()
+    
     def state_dict(self):
         return self.net.state_dict()
     
@@ -47,11 +53,11 @@ class HFCN32sModel(nn.Module):
             self,
             in_channels=3, 
             out_channels=1,
-            hebb_params=default_hebb_params
+            hebb_params=None
     ):
         super(HFCN32sModel, self).__init__()
         
-        self.hebb_params = hebb_params
+        self.hebb_params = hebb_params if hebb_params is not None else default_hebb_params
         
         # conv1
         self.conv1_1 = self._get_conv_layer(in_channels, 64, 3, padding=100)
@@ -149,6 +155,8 @@ class HFCN32sModel(nn.Module):
         self.upscore = nn.ConvTranspose2d(256, out_channels, 64, stride=32, bias=False).to(device)
     
     def forward(self, x):
+        torch.set_grad_enabled(self.hebb_params['alpha'] == 1) 
+        
         h = x
         h = self.relu1_1(self.conv1_1(h))
         h = self.relu1_2(self.conv1_2(h))
@@ -180,11 +188,14 @@ class HFCN32sModel(nn.Module):
         h = self.drop7(h)
 
         h = self.score_fr(h)
+
+        torch.set_grad_enabled(True)
         h = self.upscore(h)
         
         crop_start_x = h.shape[2] // 2 - x.shape[2] // 2
         crop_start_y = h.shape[3] // 2 - x.shape[3] // 2
         h = h[:, :, crop_start_x:crop_start_x + x.shape[2], crop_start_y:crop_start_y + x.shape[3]]
+        
         return h
 
 

train.py
@@ -73,6 +73,10 @@ def main(cfg):
         elif cfg.model.pretrained.startswith('best://'):
             checkpoint_config_path = str(run_path).replace('_ft', "")
             checkpoint_config_path = Path(re.sub("regime-\d+\.\d+", "regime-1.0", checkpoint_config_path))
+            exp_name = checkpoint_config_path.parts[-4]
+            if "base" not in exp_name:
+                new_exp_name = exp_name.split("-", 1)[0] + '_base-' + exp_name.split("-", 1)[1]
+                checkpoint_config_path = Path(str(checkpoint_config_path).replace(exp_name, new_exp_name))
             best_models_folder = checkpoint_config_path / 'best_models'
             metric_name = cfg.model.pretrained.split('best://', 1)[1]
             ckpt_path = best_models_folder / f'best_model_metric_segm-{metric_name}.pth'
@@ -80,6 +84,10 @@ def main(cfg):
         elif cfg.model.pretrained.startswith('last://'):
             checkpoint_config_path = str(run_path).replace('_ft', "")
             checkpoint_config_path = Path(re.sub("regime-\d+\.\d+", "regime-1.0", checkpoint_config_path))
+            exp_name = checkpoint_config_path.parts[-4]
+            if "base" not in exp_name:
+                new_exp_name = exp_name.split("-", 1)[0] + '_base-' + exp_name.split("-", 1)[1]
+                checkpoint_config_path = Path(str(checkpoint_config_path).replace(exp_name, new_exp_name))
             ckpt_file_name = cfg.model.pretrained.split('last://', 1)[1]
             ckpt_path = checkpoint_config_path / f'{ckpt_file_name}.pth'
             pre_trained_model = torch.load(ckpt_path, map_location=device)
@@ -88,7 +96,7 @@ def main(cfg):
             pre_trained_model = torch.load(ckpt_path, map_location=device)
         model.load_state_dict(pre_trained_model['model'])
         if cfg.model.reset_clf and hasattr(model, 'reset_clf'):
-            model.reset_clf(cfg.model.reset_clf) # Resets final classifier with number of output channels specified in cfg.model.reset_clf
+            model.reset_clf(cfg.model.reset_clf)    # Resets final classifier with number of output channels specified in cfg.model.reset_clf
         log.info(f"[PRETRAINED]: {cfg.model.pretrained}")
         
     start_epoch = 0