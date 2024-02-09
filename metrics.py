bugs fixed in dice computation

metrics.py
@@ -12,8 +12,8 @@ def _dice_jaccard_single_class(y_true, y_pred, smooth, axis):
     jaccard = (intersection + smooth) / (union + smooth)
     dice = 2. * (intersection + smooth) / (sum_ + smooth)
 
-    if y_true.sum(axis) == 0. and sum_ < smooth:
-        return torch.tensor([1.0], device=y_pred.device), torch.tensor([1.0], device=y_pred.device)
+    idx_null_intersection_elems =  torch.logical_and(y_true.sum(axis) == 0., sum_ < smooth)
+    dice[idx_null_intersection_elems] = 1.0 
 
     return dice.mean(), jaccard.mean()
 